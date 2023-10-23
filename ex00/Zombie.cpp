eebe670181941b091cf2ae10fe44353eb0ded69b
John Adithya <jadithya@42student.abudhabi.ae>
2023-10-23 12:05:12 +0400

    fixed Makefile and constructor

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,18 +6,19 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:51:19 by jadithya          #+#    #+#             */
-/*   Updated: 2023/10/23 11:41:21 by jadithya         ###   ########.fr       */
+/*   Updated: 2023/10/23 12:02:33 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie::Zombie(std::string name) {
-	std::cout << "Created a Zombie" << std::endl;
+	std::cout << "Created a Zombie: " << name << std::endl;
+	this->name = name;
 }
 
 Zombie::~Zombie(void) {
-	std::cout << "Destroyed Zombie " << this->name << std::endl;
+	std::cout << "Destroyed Zombie: " << this->name << std::endl;
 }
 
 void	Zombie::announce(void) {
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:44:20 by jadithya          #+#    #+#             */
-/*   Updated: 2023/10/23 11:47:01 by jadithya         ###   ########.fr       */
+/*   Updated: 2023/10/23 12:04:18 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int	main()
 	Zombie *z2 = newZombie("John");
 	randomChump("Bob");
 	randomChump("Charlie");
-	delete (z1);
-	delete (z2);
 	{
+		z1->announce();
+		z2->announce();
 		Zombie *z3 = newZombie("Simon");
 		randomChump("Alice");
 		delete (z3);
 	}
+	delete (z1);
+	delete (z2);
 }
