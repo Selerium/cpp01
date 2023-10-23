4b8590d68673801513b198807148a59461385732
John Adithya <jadithya@42student.abudhabi.ae>
2023-10-23 14:57:01 +0400

    added error message

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:09:56 by jadithya          #+#    #+#             */
-/*   Updated: 2023/10/23 14:19:53 by jadithya         ###   ########.fr       */
+/*   Updated: 2023/10/23 14:55:30 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 Zombie	*zombieHorde(int N, std::string name)
 {
-	if (N < 0)
+	if (N < 0) {
+		std::cout << "Incorrect input. Closing." << std::endl;
 		return (NULL);
+	}
 	Zombie	*list = new Zombie[N];
 	for (int i = 0; i < N; i++) {
 		list[i].setName(name);
